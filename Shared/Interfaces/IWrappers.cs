fb3661f4bd14c7a409b24645e912b8cf82354185
Tp <Tp@LP15>
2023-07-28 15:00:20 +0000

    Add project files.

---
+++ Shared/Interfaces/IWrappers.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Interfaces;
+
+public interface IResult
+{
+    List<string> Messages { get; set; }
+    string DetailsLink { get; set; }
+    bool Succeeded { get; set; }
+}
+
+public interface IResult<out T> : IResult
+{
+    T Data { get; }
+}
